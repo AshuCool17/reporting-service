cf630703fd1308ba5eb70752d609d4bc9366de07
Ashutosh Mahato <ashutosh.mahato@gmail.com>
2023-05-14 01:13:53 +0530

    Initial commit
    Initial commit

---
+++ ReportingApp/src/test/java/com/mycompany/reporting/ReportingApp/ReportingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.reporting.ReportingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
