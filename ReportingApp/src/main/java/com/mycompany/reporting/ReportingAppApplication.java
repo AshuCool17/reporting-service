72574b3323bbc668b1b95caa56509cdaffaa6b40
Ashutosh <ashutosh.mahato@gmail.com>
2023-05-16 23:31:28 +0530

    Added Controller

--- ReportingApp/src/main/java/com/mycompany/reporting/ReportingAppApplication.java
+++ ReportingApp/src/main/java/com/mycompany/reporting/ReportingAppApplication.java
@@ -1,4 +1,4 @@
-package com.mycompany.reporting.ReportingApp;
+package com.mycompany.reporting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ ReportingApp/src/main/java/com/mycompany/reporting/controller/ReportingController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.mycompany.reporting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gubli
+ *
+ */
+@RestController
+public class ReportingController {
+	
+	@GetMapping(value = "/reports")
+	public String reports() {
+		return null;
+	}
+
+}
